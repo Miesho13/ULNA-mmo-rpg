c4cb059a49f29dfadff3003fea0c4b40a5de8abb
Marcin <marcin.ryzewskii@gmail.com>
2024-08-05 22:24:27 +0200

    Add fzf to run.sh.

--- run.sh
+++ run.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+arg=$1
 
-cd ./build/src/client
-./application_tibia
+if [ $arg == ""]; then
+    arg=`printf "client\nsprites viewer" | fzf`
+fi
+
+if [[ $arg == "client" ]]; then
+    cd ./build/src/client
+    ./application_tibia
+elif [[ $arg == "sprites viewer" ]]; then
+    cd ./build/src/sprites_viewer
+    ./sprites_viewer
+fi
+
