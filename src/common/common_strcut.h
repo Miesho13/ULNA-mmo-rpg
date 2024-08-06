2ee38418b36101c3ed5f05ae848b777c269717f2
Marcin <marcin.ryzewskii@gmail.com>
2024-08-06 21:56:34 +0200

    Add commons_strcut

---
+++ src/common/common_strcut.h
@@ -0,0 +1,17 @@
+#ifndef __COMMON_STRUCT__
+#define __COMMON_STRUCT__
+
+#include <stdint.h>
+
+typedef struct { 
+    int64_t x;
+    int64_t y;
+} common_int64_vec;
+
+typedef struct { 
+    int32_t x;
+    int32_t y;
+} common_int32_vec;
+
+#endif
+
