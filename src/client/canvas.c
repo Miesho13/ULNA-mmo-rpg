f40eb6c7dc20990d8a0e29de6f585bd072e17425
Marcin <marcin.ryzewskii@gmail.com>
2024-08-18 19:10:07 +0200

    Delete canvas module

--- src/client/canvas.c
+++
@@ -1,9 +0,0 @@
-#include "canvas.h"
-
-void canvas_init(uint32_t posx, uint32_t posy, uint32_t with, uint32_t hight) {
-
-}
-
-void canvas_update(canvas_ctx_t ctx, canvas_drawable_descriptor_t *draw) {
-    
-}
