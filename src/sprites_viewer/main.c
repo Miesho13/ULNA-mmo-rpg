d8549184866649e849abb3b1d5c14cab9de68f38
Marcin <marcin.ryzewskii@gmail.com>
2024-08-18 19:09:17 +0200

    Refactor sprites_id_viewer

--- src/sprites_viewer/main.c
+++ src/sprites_viewer/main.c
@@ -9,8 +9,7 @@
 const int screenWidth = 800;
 const int screenHeight = 600;
 
-void print_res_with_id(Camera2D *cam) {
-    resource_context_t *ctx = resource_get_context();
+void print_res_with_id(resource_texture_t *ctx, Camera2D *cam) {
     uint32_t x = 0;
     uint32_t y = 0;
 
@@ -19,9 +18,10 @@ void print_res_with_id(Camera2D *cam) {
 
     BeginMode2D(*cam);
 
-    for (uint32_t i = 0; i < ctx->sprite_count; i++) {
-        DrawTexture(ctx->text_sprite[i], x*32*ctx->scale, y*32*ctx->scale, WHITE);
-        if ((x % 64 == 0) && (x != 0)) {
+    for (uint32_t i = 0; i < ctx->size; i++) {
+        DrawTexture(ctx->text[i], x*32*ctx->scale, y*32*ctx->scale, WHITE);
+
+        if ((x % 32 == 0) && (x != 0)) {
             y++;
             x = -1;
         }
@@ -31,10 +31,10 @@ void print_res_with_id(Camera2D *cam) {
     x = 0;
     y = 0;
     char tmp[RES_SPRITE_PER_IMG];
-    for (uint32_t i = 0; i < ctx->sprite_count; i++) {
+    for (uint32_t i = 0; i < ctx->size; i++) {
         sprintf(tmp, "0x%x", i);
-        DrawText(tmp, x * 32*ctx->scale, y * 32*ctx->scale, 5, BLACK);
-        if (x % 64 == 0 && x != 0) {
+        DrawText(tmp, x * 32*ctx->scale, y * 32*ctx->scale, 6*ctx->scale, RED);
+        if (x % 32 == 0 && x != 0) {
             y++;
             x = -1;
         }
@@ -49,10 +49,11 @@ int main(void) {
     InitWindow(screenWidth, screenHeight, "Sprite Viewer");
     SetTargetFPS(60);
     
-    reso_load_image("./resources/Sprites-%d.png", 30, 2);
+    resource_texture_t text_ctx = {0};
+    resource_load_vram(&text_ctx, "./resources/Sprites-%d.png", 30, 2);
 
     Camera2D camera = {
-        .offset = 0,
+        .offset = (Vector2){0.0f, 0.0f},
         .target = (Vector2){0.0f, 0.0f},
         .rotation = 0.0f,
         .zoom = 1.0f,
@@ -67,7 +68,7 @@ int main(void) {
         if (IsKeyDown(KEY_Z)) camera.zoom += 0.01f;
         if (IsKeyDown(KEY_X)) camera.zoom -= 0.01f;
 
-        print_res_with_id(&camera);
+        print_res_with_id(&text_ctx, &camera);
     }
     CloseWindow();
     return 0;
