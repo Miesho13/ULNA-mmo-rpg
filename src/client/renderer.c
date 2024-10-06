1d164f946ea2646bffcf3a6e40b7d662f445d9cd
Marcin <marcin.ryzewskii@gmail.com>
2024-10-06 23:59:41 +0200

    Implement part of sprite llist functionality.

--- src/client/renderer.c
+++ src/client/renderer.c
@@ -1,12 +1,62 @@
 #include "renderer.h"
+#include <stdlib.h>
 
-void renderer_update(sprite_vec_t *sprites) {
-    BeginDrawing();
-    for (uint32_t draw_id = 0; draw_id < sprites->size; draw_id++) {
-        DrawTexture(
-            *sprites->sprites[draw_id].sprite, 
-            sprites->sprites[draw_id].pos.x, 
-            sprites->sprites[draw_id].pos.y, WHITE);
-    }
-    EndDrawing();
+
+static inline void* prv_create_node(void) {
+    return calloc(1, sizeof(sprite_node));
 }
+
+static void* prv_resize_list() {
+
+}
+
+sprite_node* prv_iterate_element(sprites_list_t *llist, uint32_t index) {
+    sprite_node *current = llist->head->next;
+    for (uint32_t iter = 0; iter < index; iter++) {
+        current = current->next; 
+    }
+    return current;
+}
+
+static void prv_create_nodes(sprites_list_t *llist) {
+    llist->head = prv_create_node();
+    sprite_node *tmp = prv_create_node();
+    llist->head->next = tmp;
+
+    for (uint32_t current_node_index = 0; 
+            current_node_index < llist->size_max - 2; current_node_index++) {
+        tmp->next = prv_create_node();
+        tmp = tmp->next;
+    }
+}
+
+sprites_list_t* rend_sprites_list_init(uint32_t max_size) {
+    sprites_list_t *ret = calloc(1, sizeof(*ret));
+    ret->size_max = max_size;
+    prv_create_nodes(ret);
+
+    return ret;
+}
+
+void rend_sprites_list_add(sprites_list_t *llist, sprite_t val) {
+    if (llist->size >= llist->size_max) {
+        return;
+    }
+    
+    
+
+    llist->size++;
+
+
+}
+
+// void renderer_update(sprite_vec_t *sprites) {
+//     BeginDrawing();
+//     for (uint32_t draw_id = 0; draw_id < sprites->size; draw_id++) {
+//         DrawTexture(
+//             *sprites->sprites[draw_id].sprite, 
+//             sprites->sprites[draw_id].pos.x, 
+//             sprites->sprites[draw_id].pos.y, WHITE);
+//     }
+//     EndDrawing();
+// }
--- src/client/renderer.h
+++ src/client/renderer.h
@@ -2,19 +2,49 @@
 #define __RENDERER_H__
 
 #include <stdint.h>
+#include <stdlib.h>
 #include "raylib.h"
 
 
+//typedef struct {
+//    Texture2D *sprite;
+//    Vector2 pos;
+//} sprite_t;
+//
+//typedef struct {
+//   sprite_t *sprites;  
+//   uint32_t size;
+//} sprite_vec_t;
+//
+//
+//
+//typedef struct {
+//    sprite_t *head;
+//    sprite_t *next;
+//    size_t size; 
+//} sprite_stack_t;
+//
+//
+//void renderer_update(sprite_vec_t *vtail);
+
 typedef struct {
-    Texture2D *sprite;
+    Texture2D *texutre;
     Vector2 pos;
 } sprite_t;
 
-typedef struct {
-   sprite_t *sprites;  
-   uint32_t size;
-} sprite_vec_t;
+typedef struct sprite_node sprite_node;
+struct sprite_node {
+    sprite_node *next;
+    sprite_t val; 
+};
 
-void renderer_update(sprite_vec_t *vtail);
+typedef struct {
+    sprite_node *head;
+    uint32_t size;
+    uint32_t size_max;
+} sprites_list_t;
+
+sprites_list_t* rend_sprites_list_init(uint32_t max_size);
+sprite_node* prv_iterate_element(sprites_list_t *llist, uint32_t index);
 
 #endif
