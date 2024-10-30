39c23ac20abf23fa42d04e08fe324c9fa9b5ea66
Miesho <marcin.ryzewskii@gmail.com>
2024-10-30 15:08:25 +0100

    Update clinet tests pe.

--- src/client/main.c
+++ src/client/main.c
@@ -14,26 +14,31 @@ const int screenHeight = 600;
 int main(void) {
     InitWindow(screenWidth, screenHeight, "tibia game");
     SetTargetFPS(144);
+
     resource_texture_t text_ctx = {0};
-    resource_load_vram(&text_ctx, "./resources/Sprites-%d.png", 30, 1);
+    resource_load_vram(&text_ctx, "./resources/Sprites-%d.png", 30, 2);
     sprites_list_t *list = rend_sprites_list_init(32);
-
-    printf("head: %p\nsize: %d\nsize_max: %d\n\n", 
-            (void*)list->head, 
-            list->size, 
-            list->size_max);
     
-    for (int i = 0; i < 32; i++) {
+    int x = 0; 
+    int y = 0; 
+    for (int i = 0; i < 10; i++) {
         rend_sprites_list_add(
-            list, (sprite_t){&text_ctx.text[i+30], (Vector2){i*32, 0}}
+            list, (sprite_t){
+                &text_ctx.text[0], 
+                (Vector2){x*32*2, y*32*2}
+            }
         );
-    }
 
+        if (x++ == 4) { x = 0; y++; }
+    }
+    
+    rend_sprites_list_add(list, (sprite_t) {&text_ctx.text[1], (Vector2){1*32*2, 0*32*2}});
     while (WindowShouldClose() == false) {
+        BeginDrawing();
         ClearBackground(WHITE);
         DrawFPS(screenWidth-124, 0);
-
         rend_list(list);
+        EndDrawing();
     }
 
     CloseWindow();
