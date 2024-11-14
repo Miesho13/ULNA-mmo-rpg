8f2170ae3688bd93f7d1b0c8e73b1c5cdb7d2f52
Marcin <marcin.ryzewskii@gmail.com>
2024-11-14 22:47:02 +0100

    Change interface for renderer.

--- src/client/renderer.h
+++ src/client/renderer.h
@@ -7,6 +7,11 @@
 #include "resource.h"
 
 typedef struct {
+    uint32_t h;
+    uint32_t w;
+} image_size_px;
+ 
+typedef struct {
     Texture2D *texutre;
     Vector2 pos;
 } sprite_t;
@@ -25,15 +30,18 @@ typedef struct {
 } render_vector;
 
 typedef struct {
-    resource_texture_t resource_ctx;
+    Image *image_buffer;
+     
 } renderer_ctx;
 
 
-void renderer_init(resource_texture_t *renderer_ctx);
+void renderer_init(renderer_ctx *rctx, const char *pattern, 
+                   uint32_t sheet_count, uint32_t scale);
+
 render_vector*  render_vecotr_init(uint32_t max_size);
 void            render_vecotr_push(render_vector *vec, sprite_t val);
 sprite_t        render_vecotr_get(render_vector *vec, uint32_t index);
 sprite_t        render_vecotr_pop(render_vector *vec, uint32_t index);
-void            render_buffer(sprites_list_t *list);
+void            render_buffer(renderer_ctx *rctx, render_vector *list);
 #endif
 
