13d538be4eaab496f72e0c59fb2d1c21701db718
Marcin <marcin.ryzewskii@gmail.com>
2024-12-01 12:57:31 +0100

    Add value for rendered structure.

--- src/client/renderer.h
+++ src/client/renderer.h
@@ -31,17 +31,27 @@ typedef struct {
 
 typedef struct {
     Image *image_buffer;
-     
-} renderer_ctx;
+    size_t image_buff_size;
+
+    Texture *text_buffer;
+    size_t text_buff_size;
+
+    char *path_to_sprites_sheet;
+    uint32_t scale;
+
+} renderer_ctx_t;
 
 
-void renderer_init(renderer_ctx *rctx, const char *pattern, 
+void renderer_init(renderer_ctx_t *rctx, const char **pattern, 
                    uint32_t sheet_count, uint32_t scale);
+
+void renderer_load_text(renderer_ctx_t *rctx);
+void renderer_free_text(renderer_ctx_t *rctx);
 
 render_vector*  render_vecotr_init(uint32_t max_size);
 void            render_vecotr_push(render_vector *vec, sprite_t val);
 sprite_t        render_vecotr_get(render_vector *vec, uint32_t index);
 sprite_t        render_vecotr_pop(render_vector *vec, uint32_t index);
-void            render_buffer(renderer_ctx *rctx, render_vector *list);
+void            render_buffer(renderer_ctx_t *rctx, render_vector *list);
 #endif
 
