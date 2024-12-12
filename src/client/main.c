ea976e0a1a430a781ca18f8be08a869e3074a110
Marcin <marcin.ryzewskii@gmail.com>
2024-12-12 19:28:47 +0100

    Update entry point for client

--- src/client/main.c
+++ src/client/main.c
@@ -5,8 +5,8 @@
 #include <stdio.h>
 
 #include "raylib.h"
-#include "resource.h"
 #include "renderer.h"
+#include "sprite_data.h"
 
 const int screenWidth = 800;
 const int screenHeight = 600;
@@ -14,13 +14,23 @@ const int screenHeight = 600;
 int main(void) {
     InitWindow(screenWidth, screenHeight, "tibia game");
     SetTargetFPS(144);
-    
-    renderer_ctx rctx;
-    renderer_init(&rctx, "resources/Sprites-%d.png", 30, 1);
+
+    renderer_ctx_t rctx;
+    renderer_init(&rctx, &game_sprite_load_ctx, 1);
 
     while (WindowShouldClose() == false) {
         BeginDrawing();
-        ClearBackground(WHITE);
+        ClearBackground(SKYBLUE);
+
+        int32_t x = 0;
+        int32_t y = 0;
+
+        for (uint32_t i = 0; i < 12*12; i++) {
+            DrawTexture(rctx.text_buffer[i], x*32, y*32, WHITE);
+            x++;
+            if (x == 12 ) { x = 0; y++; }
+        }
+
         DrawFPS(screenWidth-124, 0);
         EndDrawing();
     }
