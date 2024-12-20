b0e441b0d3dbd531f51585b06a3cb6d0e19ecb3a
Marcin <marcin.ryzewskii@gmail.com>
2024-12-20 13:37:10 +0100

    Fix the name issue in run.sh

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 arg=$1
 
 if [ $arg == ""]; then
-    arg=`printf "client\nsprites viewer" | fzf`
+    arg=`printf "client\nsprites_viewer" | fzf`
 fi
 
 if [[ $arg == "client" ]]; then
     cd ./build/src/client
     ./application_tibia
-elif [[ $arg == "sprites view" ]]; then
+elif [[ $arg == "sprites_viewer" ]]; then
     cd ./build/src/sprites_viewer
     ./sprites_viewer
 fi
