37f507156335d12ebe38683844bdb029484b79a5
Marcin <marcin.ryzewskii@gmail.com>
2024-12-24 08:22:01 +0100

    Refactor structure of renderer.

--- src/client/renderer.h
+++ src/client/renderer.h
@@ -4,32 +4,17 @@
 #include <stdint.h>
 #include <stdlib.h>
 
+#include "dynamic_array.h"
 #include "raylib.h"
 #include "sprite_data.h"
 #include "sprite_data.h"
 
-typedef struct {
-    uint32_t h;
-    uint32_t w;
-} image_size_px;
+typedef vec_array vec_render_obj;
 
 typedef struct {
-    Texture2D *texutre;
     Vector2 pos;
-} sprite_t;
-
-typedef struct sprite_node sprite_node;
-struct sprite_node {
-    sprite_node *next;
-    sprite_t val;
-};
-
-typedef struct {
-    sprite_node *head;
-    uint32_t size;
-    uint32_t size_max;
-    uint32_t counter_end;
-} render_vector;
+    uint32_t id;
+} render_obj;
 
 typedef struct {
     Image *image_buffer;
@@ -48,15 +33,12 @@ typedef struct {
 
 } renderer_ctx_t;
 
+void renderer_init(
+    renderer_ctx_t *rctx, 
+    sprite_loader_path_t *sprite_data, 
+    double scale
+);
 
-void renderer_init(renderer_ctx_t *rctx, sprite_loader_path_t *sprite_data, double scale);
-
-void renderer_load_text(renderer_ctx_t *rctx);
-void renderer_free_text(renderer_ctx_t *rctx);
-
-render_vector*  render_vecotr_init(uint32_t max_size);
-void            render_vecotr_push(render_vector *vec, sprite_t val);
-sprite_t        render_vecotr_get(render_vector *vec, uint32_t index);
-sprite_t        render_vecotr_pop(render_vector *vec, uint32_t index);
-void            render_buffer(renderer_ctx_t *rctx, render_vector *list);
+void rend_init(renderer_ctx_t *rctx, sprite_loader_path_t *sheet_data, double scale);
+void rend_draw_vector(const renderer_ctx_t *rctx, const vec_array objects_v);
 #endif
