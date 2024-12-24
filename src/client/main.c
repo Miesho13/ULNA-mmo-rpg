a636c3237896a6ed5faf0fd5402677ecb886d080
Marcin <marcin.ryzewskii@gmail.com>
2024-12-24 08:25:57 +0100

    Change client entry point.

--- src/client/main.c
+++ src/client/main.c
@@ -4,10 +4,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <stdio.h>
 
 #include "raylib.h"
 #include "renderer.h"
 #include "sprite_data.h"
+#include "dynamic_array.h"
+
 
 const int screenWidth = 800;
 const int screenHeight = 600;
@@ -21,48 +24,40 @@ bool has_time_elapsed(float *lastTime, float interval) {
     return false;
 }
 
+
+
 int main(void) {
     InitWindow(screenWidth, screenHeight, "tibia game");
     SetTargetFPS(144);
-
+    
     renderer_ctx_t rctx;
-    renderer_init(&rctx, &game_sprite_load_ctx, 1.5);
+    rend_init(&rctx, &game_sprite_load_ctx, 1.7);
 
     int animatein_start = (12*12)*3 - 11;
-    int animation_count = animatein_start;
     float last_time = 0.0f;
     float interval = 0.2f;
-
-    int animation_frame[] = {
-        animatein_start,
-        animatein_start + 1,
-        animatein_start,
-        animatein_start + 1,
-        animatein_start,
-        animatein_start + 1,
-        animatein_start + 2,
-        animatein_start + 3,
-    };
+    
+    vec_array texts_vec = vec_array_new(20, sizeof(render_obj));
+    Vector2 posytion = {0};
+    for (uint32_t i = 0; i < vec_capasity(texts_vec); i++) {
+        vec_push(texts_vec, &((render_obj){(Vector2){posytion.x*rctx.sprite_width, posytion.y*rctx.sprite_height}, 0}));
+        posytion.x += 1.f;
+        if (posytion.x == 6.f) {
+            posytion.y += 1.f; 
+            posytion.x = 0;
+        }
+    }
 
     uint32_t i = 0;
     while (WindowShouldClose() == false) {
         BeginDrawing();
         ClearBackground(SKYBLUE);
 
-        int32_t x = 0;
-        int32_t y = 0;
-
-        // for (uint32_t i = 0; i < 12*12; i++) {
-        //     DrawTexture(rctx.text_buffer[i], x*32, y*32, WHITE);
-        //     x++;
-        //     if (x == 12 ) { x = 0; y++; }
-        // }
-
-        DrawTexture(rctx.text_buffer[animation_frame[i % 8]], 400, 300, WHITE);
-
         if (has_time_elapsed(&last_time, interval)) {
             i++;
         }
+    
+        rend_draw_vector(&rctx, texts_vec);
 
         DrawFPS(screenWidth-124, 0);
         EndDrawing();
