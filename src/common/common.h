076793c8163c56e143cd4f7d1460a6fe06b8fc30
miesho <marcin.ryzewskii@gmail.com>
2025-09-25 21:07:35 +0200

    src/common/common: add common instance for common stuf

---
+++ src/common/common.h
@@ -0,0 +1,19 @@
+#ifndef ULNA_COMMON_H
+#define ULNA_COMMON_H
+
+#include <assert.h>
+#include <stdio.h>
+
+#define COMMON_NOT_IMPL_YET() assert(false && "Do not implemented yet")
+
+
+// ---------- LOGS ----------------
+#define INFO  "[INFO]" 
+#define ERROR "[ERROR]" 
+
+#define LOG(LEVEL, msg, ...) printf(LEVEL" "msg"\n", ##__VA_ARGS__)
+
+// ---------- END LOGS ----------------
+
+#endif //ULNA_COMMON_H
+
