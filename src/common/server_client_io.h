2398f62f6adb9347b58ecf77853df99f698335ed
miesho <marcin.ryzewskii@gmail.com>
2025-09-28 23:36:33 +0200

    common/server_client_io: add update_respone

--- src/common/server_client_io.h
+++ src/common/server_client_io.h
@@ -7,6 +7,7 @@ typedef enum  {
     HELLO = 0x11,
     MOVE = 0x1,
     IDLE = 0x11,
+    UPDATE = 0x69,
 
     GOOD_BYE = 0xfd,
     NONE = 0xff,
@@ -23,17 +24,17 @@ typedef struct {
 
 typedef struct {
     uint8_t head; 
-    uint32_t id;
+    uint16_t id;
 } register_respone_t;
 
 typedef struct {
     uint8_t head; 
-    uint32_t id;
+    uint16_t id;
 } good_bye_request_t;
 
 typedef struct {
     uint8_t head; 
-    uint32_t id;
+    uint16_t id;
     struct {
         int dx; 
         int dy; 
@@ -48,6 +49,26 @@ typedef struct {
     } pos;
     uint8_t players[11*11];
 } event_respone_t;
+
+#define PLAYER_MAP_SIZE 11*11
+#define EVENT_MAX_SIZE  64
+
+typedef struct {
+    uint16_t who_id;
+    uint16_t to_whom;
+    uint8_t action;
+} event;
+
+typedef struct {
+    uint8_t head; 
+    struct {
+        int x; 
+        int y; 
+    } pos;
+    uint16_t player_map[PLAYER_MAP_SIZE]; 
+    uint8_t npc_map[PLAYER_MAP_SIZE]; 
+    event   action[EVENT_MAX_SIZE]; 
+} update_respone;
 
 // HELPER FUNCTION
 
