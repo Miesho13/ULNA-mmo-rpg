9676531115582e6130a0866ef49273035ad394e2
miesho <marcin.ryzewskii@gmail.com>
2026-03-15 11:14:09 +0100

    common: expand type.h with fixed-width typedefs and vector2 types

--- src/common/type.h
+++ src/common/type.h
@@ -1,9 +1,49 @@
-#ifndef __COMMON_TYPE__
-#define __COMMON_TYPE__
+#ifndef __TYPE__
+#define __TYPE__
+
+#include <stdint.h>
+
+typedef uint8_t   u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t   i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef float  f32;
+typedef double f64;
 
 typedef struct {
-    int x;
-    int y;
-} vector2_int_t;
+    u8 x;
+    u8 y;
+} vector2_u8;
 
-#endif
+typedef struct {
+    u16 x;
+    u16 y;
+} vector2_u16;
+
+typedef struct {
+    u32 x;
+    u32 y;
+} vector2_u32;
+
+typedef struct {
+    u64 x;
+    u64 y;
+} vector2_u64;
+
+typedef struct {
+    f32 x;
+    f32 y;
+} vector2_f32;
+
+typedef struct {
+    f64 x;
+    f64 y;
+} vector2_f64;
+
+#endif // __TYPE__
