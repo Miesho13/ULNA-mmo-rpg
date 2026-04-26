e583f259c45c3919e2236c4f9c9995450c0e6b23
miesho <marcin.ryzewskii@gmail.com>
2026-04-26 17:22:41 +0200

    client: update test scene

--- src/client/scene/render_test_scene.c
+++ src/client/scene/render_test_scene.c
@@ -6,13 +6,25 @@
 #include "server_client_io.h"
 #include "../common/time_bench.h"
 #include "../renderer.h"
+#include "../tile.h"
+#include "../client_config.h"
+#include "../ui.h"
 
 #include <string.h>
 #include <unistd.h>
 
+
 typedef struct {
     bool is_init;
     app_context *app;
+
+    struct {
+        struct {
+            int x;
+            int y;
+        } game_pos;
+
+    } player_context; 
 
     struct {
         int x;
@@ -29,6 +41,20 @@ typedef struct {
         int dy;
     } dpos;
 
+
+    tile_plane tile_ctx;
+    ui_context ui_context;
+
+    struct {
+        render_sprite_vector sprite_context;
+        platform_sprite background[TILES_PER_PLANE];
+    } render_buffers;
+
+    struct {
+        uint32_t background[TILES_PER_PLANE];
+        uint32_t npc[TILES_PER_PLANE];
+    } layers;
+
     event_t event;
     event_t event_to_handle[EVENT_MAX_SIZE];
     map_grid player_tile_map[MAIN_SCENE_TILE_BUFF_SIZE];
@@ -36,80 +62,12 @@ typedef struct {
     struct {
         bool debug_dump_info;
         double server_comm_delay;
+        double plain_load_us;
     } bench;
 
 } main_scene_context;
 
 static main_scene_context CORE;
-
-static inline void log_in_to_server(void) {
-    CORE.app->client_context.id = hello_to_server();
-    if (CORE.app->client_context.id < 0) {
-        CORE.app->client_context.login = false; 
-        LOG(COMMON_LOG_ERROR, "We cannot log in to the game server.");
-    }
-    CORE.app->client_context.login = true; 
-}
-
-static inline void clear_after_send() {
-    // HERE SHOUD BE RESET ALL SATE WHAT WE NEED TO SEND ONCE 
-    CORE.dpos.dx = 0;
-    CORE.dpos.dy = 0;
-
-    memset(&CORE.event, 0, sizeof(CORE.event));
-}
-
-static inline void server_communication(void) {
-    if (CORE.app->client_context.login == false) {
-        return;
-    }
-
-    time_start();
-    
-    update_request_t update_request = {0};
-
-    update_request.head    = UPDATE;
-    update_request.id      = CORE.app->client_context.id;
-    update_request.dpos.dx = CORE.dpos.dx;
-    update_request.dpos.dy = CORE.dpos.dy;
-    update_request.event   = CORE.event;
-
-    client_push_event(&update_request);
-
-    // TODO: Need to make some lifetime and timestamp for comunication
-    //       if some recv dont come we shoud make recive 
-  
-    // handle server respone 
-    update_respone_t update_respone = {0};
-    client_recv_update_event(&update_respone);
-    
-    memcpy(CORE.event_to_handle, update_respone.events, sizeof(event_t)*EVENT_MAX_SIZE);
-    memcpy(CORE.player_tile_map, update_respone.player_map, 
-           sizeof(map_grid) * MAIN_SCENE_TILE_BUFF_SIZE);
-
-    clear_after_send();
-
-    CORE.bench.server_comm_delay = time_end_ns() / 1000000.f;
-
-    printf("INCOMING DATA:\n");
-        printf("\tupdate_respone.events:\n");
-    for (int i = 0; i < 12; i++) {
-        printf("\tevent: [%d %d (%d %d)]\n", 
-                update_respone.events[i].action, 
-                update_respone.events[i].player_id, 
-                update_respone.events[i].relative_pos.x,
-                update_respone.events[i].relative_pos.y);
-    }
-    printf("update_respone.head %d\n", update_respone.head);
-    printf("update_respone.pos.x %d\n", update_respone.pos.x);
-    printf("update_respone.pos.y %d\n", update_respone.pos.y);
-}
-
-/* Hanel an users's inputs */ 
-static inline void set_dpos(int dx, int dy) {
-    CORE.dpos.dx = dx; 
-    CORE.dpos.dy = dy; 
-}
 
 /*
  * Checks the current state of the keyboard for movement keys
@@ -124,21 +82,22 @@ static inline void set_dpos(int dx, int dy) {
 static inline void position_change_event(void) {
     // TODO: Need some sample rate how many offen we read the imput form user 
     
-    if (platform_key_down(PLATFORM_KEY_A) ||  
-            platform_key_down(PLATFORM_KEY_LEFT)) {
-        set_dpos(-1, 0);
+    if (platform_key_press(PLATFORM_KEY_A) ||  
+            platform_key_press(PLATFORM_KEY_LEFT)) {
+        CORE.player_context.game_pos.x -= 1;
     }
-    else if (platform_key_down(PLATFORM_KEY_D) ||  
-            platform_key_down(PLATFORM_KEY_RIGHT)) {
-        set_dpos(1, 0);
+    else if (platform_key_press(PLATFORM_KEY_D) ||  
+            platform_key_press(PLATFORM_KEY_RIGHT)) {
+        CORE.player_context.game_pos.x += 1;
+
     }
-    else if (platform_key_down(PLATFORM_KEY_W) ||  
-            platform_key_down(PLATFORM_KEY_UP)) {
-        set_dpos(0, -1);
+    else if (platform_key_press(PLATFORM_KEY_W) ||  
+            platform_key_press(PLATFORM_KEY_UP)) {
+        CORE.player_context.game_pos.y -= 1;
     }
-    else if (platform_key_down(PLATFORM_KEY_S) ||  
-            platform_key_down(PLATFORM_KEY_DOWN)) {
-        set_dpos(0, 1);
+    else if (platform_key_press(PLATFORM_KEY_S) ||  
+            platform_key_press(PLATFORM_KEY_DOWN)) {
+        CORE.player_context.game_pos.y += 1;
     }
 }
 
@@ -157,8 +116,12 @@ static inline void mose_input_event(void) {
 }
 
 static inline void input_event(void) {
-    // position_change_event();
+     position_change_event();
     // mose_input_event();
+
+    if (platform_key_press(PLATFORM_KEY_P)) {
+        ui_remove(&CORE.ui_context, 1);
+    }
 
     if (platform_key_press(PLATFORM_KEY_N)) {
         CORE.bench.debug_dump_info ^= 1;
@@ -173,29 +136,78 @@ static inline void drump_deb_info(bool dumb) {
     v2_i32 pos = platform_get_mause();
 
     char ms[3*64] = {0};
-    sprintf(ms, "ms: %5.2f\n"
-                "player_id: %d\n"
-                "mause_pos: (%d, %d)\n",
-            CORE.bench.server_comm_delay,
-            CORE.app->client_context.id,
-            pos.x/CORE.tail_size.w, pos.y/CORE.tail_size.h);
+
+    sprintf(ms,// "ms: %5.2f\n"
+               // "player_id: %d\n"
+               // "mause_pos: (%d, %d)\n"
+               "parse plain: (%f us)\n",
+               // CORE.bench.server_comm_delay,
+               // CORE.app->client_context.id,
+               // pos.x/CORE.tail_size.w, pos.y/CORE.tail_size.h,
+               CORE.bench.plain_load_us/1000000);
 
     platform_draw_text(ms, 0, 0, 20, 0x2bf21dff);
 }
 
-static inline void render_plain(void) {
-    platform_draw_prolog();
-    platform_clear_background(0x4d4f8cff);
+static void render_background(void) 
+{
+    for (int id = 0; id < TILES_PER_PLANE; ++id) {
+        CORE.layers.background[id] = (3*RENDER_PLAIN_W+5);
+    }
 
-    sprites_t* sprite_ctx = get_sprite();
-
-
-    platform_render(&sprite_ctx->sprite_buffer[24], 160, 160, 0xffffffff);
-    
-    platform_draw_epilog();
+    tile_render_layer(&CORE.tile_ctx, CORE.layers.background);
 }
 
-static inline void init(app_context *app) {
+static void render_ui(void) 
+{
+    // platform_render(&CORE.ui_context.asset_gpu[0], 0, 0, 0xFFFFFFFF);
+    ui_render(&CORE.ui_context);
+}
+
+static inline void render_frame(void) 
+{
+    // platform_draw_prolog();
+    // platform_clear_background(0x4d4f8cff);
+    // render_background();
+
+    // drump_deb_info(CORE.bench.debug_dump_info);
+    // platform_draw_epilog();
+    
+     platform_draw_prolog();
+     platform_clear_background(0x4d4f8cff);
+
+     platform_draw_line(10, 10, 100, 100, 2.0, 0xFF0000FF);
+
+     platform_relative_mode_prolog(100, 300); {
+         platform_draw_line(10, 10, 100, 100, 2.0, 0xFFFF00FF);
+         platform_draw_line(100, 100, 150, 50, 2.0, 0xFFFF00FF);
+     } platform_relative_mode_epilog();
+
+     platform_draw_line(100, 100, 150, 50, 2.0, 0xFF0000FF);
+
+     platform_draw_epilog();
+}
+
+static void load_plains(void) 
+{
+    const char tmp[255] = {0};
+
+    for (int i = 0; i < 30; i++) {
+        sprintf((char*)tmp, "./../resources/7.1/Sprites-%d.png", i);
+
+        render_load_sprite_from_plain(
+            &CORE.render_buffers.sprite_context, tmp,
+            (v2_i32){.x = 32, .y = 32}, 2.0
+        );
+    }
+
+    ui_load_assets(&CORE.ui_context, "./../ui-assets/ui-plane.png");
+}
+
+// Initializes core application settings and context. Prepares the necessary resources,
+// configures initial states, and sets up user interface elements for the application.
+static inline void init(app_context *app) 
+{
     if (CORE.is_init) {
         return;
     }
@@ -203,13 +215,25 @@ static inline void init(app_context *app) {
     if (app) {
         CORE.app = app;
     }
-    
-    render_load_sprite_from_plain("./../resources/7.0/Sprites-0.png", (v2_i32){.x = 32, .y = 32});
-         
+
+    time_start();
+    load_plains();
+    CORE.bench.plain_load_us = (float)time_end_ns() / 1000000;
+
+    CORE.tile_ctx.layer_h = TILES_H;
+    CORE.tile_ctx.layer_w = TILES_W;
+    CORE.tile_ctx.sprite_size.h = CORE.render_buffers.sprite_context.sprite[0].height;
+    CORE.tile_ctx.sprite_size.w = CORE.render_buffers.sprite_context.sprite[0].width;
+    CORE.tile_ctx.sprites = &CORE.render_buffers.sprite_context;
+
+    CORE.player_context.game_pos.x = 20;
+    CORE.player_context.game_pos.y = 20;
+
     CORE.is_init = true;
 } 
 
-void render_test_scene(app_context *app) {
+void render_test_scene(app_context *app) 
+{
     if (platform_window_shoud_close()) {
         app->game_close = true;
         return;
@@ -222,8 +246,7 @@ void render_test_scene(app_context *app) {
     init(app);
 
     // handle all input events from the user 
-    input_event();
-    render_plain();
+    // input_event();
+    render_frame();
 
-    // render scene
 }
